a88fc28c339049c4bfd44f84859373b9dc5b1f1b
Nick McIntosh <nick@nickmcintosh>
2016-02-25 15:47:43 +1100

    Fork from meteor's facebook, with added profilePictureURL

---
+++ README.md
@@ -0,0 +1,5 @@
+# facebook
+
+Updated from the Meteor package `facebook` to also include the user's `profilePictureURL` on the root user object.
+
+An implementation of the Facebook OAuth flow. See the [project page](https://www.meteor.com/accounts) on Meteor Accounts for more details. XXX link
---
+++ package.js
@@ -0,0 +1,24 @@
+Package.describe({
+  name: 'heysailor:facebook',
+  summary: "Facebook OAuth flow",
+  version: "1.2.3"
+});
+
+Package.onUse(function(api) {
+  api.use('oauth2', ['client', 'server']);
+  api.use('oauth', ['client', 'server']);
+  api.use('http', ['server']);
+  api.use('templating', 'client');
+  api.use('underscore', 'server');
+  api.use('random', 'client');
+  api.use('service-configuration', ['client', 'server']);
+
+  api.export('Facebook');
+
+  api.addFiles(
+    ['facebook_configure.html', 'facebook_configure.js'],
+    'client');
+
+  api.addFiles('facebook_server.js', 'server');
+  api.addFiles('facebook_client.js', 'client');
+});
