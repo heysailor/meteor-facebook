ab371907bbdf25eda790a4dbd5835e6c4a569695
Nick McIntosh <nick@nickmcintosh>
2016-02-25 15:56:22 +1100

    Dependencies versioning for package publishing

--- package.js
+++ package.js
@@ -5,13 +5,13 @@ Package.describe({
 });
 
 Package.onUse(function(api) {
-  api.use('oauth2', ['client', 'server']);
-  api.use('oauth', ['client', 'server']);
-  api.use('http', ['server']);
-  api.use('templating', 'client');
-  api.use('underscore', 'server');
-  api.use('random', 'client');
-  api.use('service-configuration', ['client', 'server']);
+  api.use('oauth2@1.1.5', ['client', 'server']);
+  api.use('oauth@1.1.6', ['client', 'server']);
+  api.use('http@1.1.1', ['server']);
+  api.use('templating@1.1.5', 'client');
+  api.use('underscore@1.0.4', 'server');
+  api.use('random@1.0.5', 'client');
+  api.use('service-configuration@1.0.5', ['client', 'server']);
 
   api.export('Facebook');
 
